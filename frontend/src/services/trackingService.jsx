cecf4393307eedad4fb650fae0476f8ca4dc7bbb
Rizka Aulia <rizkauliaa2715@gmail.com>
2026-06-15 19:33:46 +0700

    Update trackingService.jsx

--- frontend/src/services/trackingService.jsx
+++ frontend/src/services/trackingService.jsx
@@ -1,18 +1,18 @@
-import axios from "axios";
+// import axios from "axios";
 
-const trackingApi = axios.create({
-  baseURL: "http://localhost:8087",
-});
+// const trackingApi = axios.create({
+//   baseURL: "http://localhost:8087",
+// });
 
-const createTracking = async (data) => {
-  const response = await trackingApi.post(
-    "/tracking",
-    data
-  );
+// const createTracking = async (data) => {
+//   const response = await trackingApi.post(
+//     "/tracking",
+//     data
+//   );
 
-  return response.data;
-};
+//   return response.data;
+// };
 
-export default {
-  createTracking,
-};
+// export default {
+//   createTracking,
+// };
