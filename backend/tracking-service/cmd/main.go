342b6cebd491c8a887c4f415be518eb005e439bf
Rizka Aulia <rizkauliaa2715@gmail.com>
2026-06-15 19:34:43 +0700

    Update main.go

--- backend/tracking-service/cmd/main.go
+++ backend/tracking-service/cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"time"
+
 	"tracking-service/internal/config"
 	"tracking-service/internal/handler"
 	"tracking-service/internal/repository"
 	"tracking-service/internal/routes"
 	"tracking-service/internal/service"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,6 +18,22 @@ func main() {
 	config.ConnectDB()
 
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"http://localhost:5173",
+		},
+		AllowMethods: []string{
+			"GET", "POST", "PUT", "DELETE", "OPTIONS",
+		},
+		AllowHeaders: []string{
+			"Origin",
+			"Content-Type",
+			"Authorization",
+		},
+		AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
+	}))
 
 	trackingRepo :=
 		repository.NewTrackingRepository(
