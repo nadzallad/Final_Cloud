ff8f02f8f6991ac4b33cfe3cecf0d65972e93987
Rizka Aulia <rizkauliaa2715@gmail.com>
2026-06-16 21:33:11 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import NotificationListener from "./components/NotificationListener";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Dashboard from "./pages/Dashboard";
 import Orders from "./pages/Orders";
 import CreateOrder from "./pages/CreateOrder";
 import Tracking from "./pages/Tracking";
+import TrackingUser from "./pages/TrackingUser";
 import Profile from "./pages/Profile";
 import Payment from "./pages/Payment";
 import Pickups from "./pages/Pickups";
@@ -11,18 +15,23 @@ import Warehouse from "./pages/Warehouse";
 import Shipment from "./pages/Shipment";
 
 function App() {
+  <>
+    <NotificationListener />
+    <ToastContainer />
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/orders" element={<Orders />} />
       <Route path="/orders/create" element={<CreateOrder />} />
       <Route path="/tracking" element={<Tracking />} />
+      <Route path="/tracking-user" element={<TrackingUser />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/payment" element={<Payment />}/>
       <Route path="/pickups" element={<Pickups />}/>
       <Route path="/warehouse" element={<Warehouse />}/>
       <Route path="/shipment" element={<Shipment />} />
     </Routes>
+  <?
   );
 }
 
